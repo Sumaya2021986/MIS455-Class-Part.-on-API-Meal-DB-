2d8164cb96e5a5c0d21eb5ff9b07d3b01412231f
Sumaya2021986 <126961709+Sumaya2021986@users.noreply.github.com>
2023-12-05 01:19:40 +0600

    Add files via upload

---
+++ meal_api/mealDb.js
@@ -0,0 +1,51 @@
+
+function connect(){
+
+ var searchTerm = document.getElementById("searchBox").value ; 
+
+ 
+  // Clear the input field
+  document.getElementById("searchBox").value = "";
+
+
+ var url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchTerm}` ;
+ console.log (url);
+
+ fetch (url)
+ .then (res => res.json())
+ .then (data => display(data))
+
+}
+
+function display (data){
+
+  //console.log (data);
+  var allMeals = data.meals ;
+  console.log(allMeals);
+
+  var oldContent = document.getElementById("container");
+ 
+  // Clear previous content
+ oldContent.textContent = "";
+
+
+  for (var i=0; i < Math.min(allMeals.length, 5); i++){
+
+    var newDiv = document.createElement("div");
+    newDiv.innerHTML = `Meal Name: ${allMeals[i].strMeal}  <br>
+                        Meal ID: ${allMeals[i].idMeal}<br>
+                        <img src="${allMeals[i].strMealThumb}"> <br> <br>
+                        Cooking Instructions: <p>
+                        ${allMeals[i].strInstructions}  </p> <br>
+                        ` 
+  
+
+     newDiv.classList.add("mealStyle");
+     oldContent.appendChild(newDiv);
+     
+
+    
+  }
+     
+
+}
